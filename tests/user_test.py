22d46e850f51545b451aeee83a7964d5bac55039
Kubi <iamkubi@gmail.com>
2020-04-12 01:22:29 -0700

    Add unittests for delete user

--- tests/user_test.py
+++ tests/user_test.py
@@ -47,13 +47,29 @@ class UserTests(unittest.TestCase):
             self.client.user.get_user_info(user_id=1, external_id=2)
 
     @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
-    def test_delete_user(self, mock_api):
+    def test_delete_user_with_user_id(self, mock_api):
         expected = {
             'endpoint': 'application/users/77',
             'mode': 'DELETE',
         }
         self.client.user.delete_user(user_id=77)
         mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_delete_user_with_external_id(self, mock_api):
+        mock_api.return_value = {'attributes': {'id': 88}}
+        expected = [mock.call(endpoint='application/users/external/11'),
+                    mock.call(endpoint='application/users/88', mode='DELETE')]
+        self.client.user.delete_user(external_id=11)
+        mock_api.assert_has_calls(expected)
+
+    def test_delete_user_raises_with_no_id(self):
+        with self.assertRaises(BadRequestError):
+            self.client.user.delete_user()
+
+    def test_delete_user_raises_with_both_ids(self):
+        with self.assertRaises(BadRequestError):
+            self.client.user.delete_user(user_id=1, external_id=2)
 
 
 if __name__ == '__main__':
