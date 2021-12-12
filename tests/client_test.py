5009e2989d898459f5093b7af7e128c6447d31d8
iamkubi <iamkubi@gmail.com>
2021-12-11 23:26:19 -0800

    Add tests for missing Client methods (#42)

--- tests/client_test.py
+++ tests/client_test.py
@@ -56,6 +56,84 @@ class ClientTests(unittest.TestCase):
         self.client.client.send_power_action(1, 'start')
         mock_api.assert_called_with(**expected)
 
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_list_files(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/1/files/list',
+            'params': {},
+        }
+        self.client.client.list_files(1)
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_list_files_with_path(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/1/files/list',
+            'params': {'directory': '/foo'}
+        }
+        self.client.client.list_files(1, '/foo')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_download_file(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/2/files/download',
+            'params': {'file': 'backups/savegame.zip'},
+        }
+        self.client.client.download_file(2, 'backups/savegame.zip')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_rename_file(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/3/files/rename',
+            'mode': 'PUT',
+            'data': {'root': '/', 'files': [{'from': 'today.zip',
+                                             'to': 'yesterday.zip'}]},
+        }
+        self.client.client.rename_file(3, 'today.zip', 'yesterday.zip')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_rename_file_with_root(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/3/files/rename',
+            'mode': 'PUT',
+            'data': {'root': 'backups',
+                     'files': [{'from': 'today.zip',
+                                'to': 'yesterday.zip'}]},
+        }
+        self.client.client.rename_file(3, 'today.zip', 'yesterday.zip',
+                                       'backups')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_list_api_keys(self, mock_api):
+        expected = {
+            'endpoint': 'client/account/api-keys',
+        }
+        self.client.client.list_api_keys()
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_create_api_key(self, mock_api):
+        expected = {
+            'endpoint': 'client/account/api-keys',
+            'mode': 'POST',
+            'data': {'description': 'Test key', 'allowed_ips': ["127.0.0.1"]},
+        }
+        self.client.client.create_api_key('Test key', ["127.0.0.1"])
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_delete_api_key(self, mock_api):
+        expected = {
+            'endpoint': 'client/account/api-keys/abc123',
+            'mode': 'DELETE',
+        }
+        self.client.client.delete_api_key('abc123')
+        mock_api.assert_called_with(**expected)
+
 
 if __name__ == '__main__':
     unittest.main()
