30725dfa686072104e2f3a5cefa247796c453c39
iamkubi <iamkubi@gmail.com>
2021-12-12 21:25:43 -0800

    Add client database APIs

--- pydactyl/api/client/servers/databases.py
+++ pydactyl/api/client/servers/databases.py
@@ -20,14 +20,43 @@ class Databases(base.PterodactylAPI):
         response = self._api_request(endpoint=endpoint, params=params)
         return response
 
-    def create_database(self):
-        """TODO"""
-        pass
+    def create_database(self, server_id: str, name: str, remote: str = '%'):
+        """Create a new database for the specified server.
 
-    def rotate_database_password(self):
-        """TODO"""
-        pass
+        Limits connections to the address specified in remote, defaulting to
+        allowing from all.
 
-    def delete_database(self, database_id):
-        """TODO"""
-        pass
+        Args:
+            server_id(str): Server identifier (abbreviated UUID)
+            remote(str): Remote address to allow connections from (e.g. 1.2.3.4)
+        """
+        data = {'database': name, 'remote': remote}
+        endpoint = 'client/servers/{}/databases'.format(server_id)
+        response = self._api_request(endpoint=endpoint, mode='POST', data=data)
+        return response
+
+    def rotate_database_password(self, server_id: str, database_id: str):
+        """Changes the password of the specified database.
+
+        Generates a new password and returns it in the response.
+
+        Args:
+            server_id(str): Server identifier (abbreviated UUID)
+            database_id(str): Database identifier (long UUID)
+        """
+        endpoint = 'client/servers/{}/databases/{}/rotate-password'.format(
+            server_id, database_id)
+        response = self._api_request(endpoint=endpoint, mode='POST')
+        return response
+
+    def delete_database(self, server_id: str, database_id: str):
+        """Deletes the specified database.
+
+        Args:
+            server_id(str): Server identifier (abbreviated UUID)
+            database_id(str): Database identifier (long UUID)
+        """
+        endpoint = 'client/servers/{}/databases/{}'.format(server_id,
+                                                           database_id)
+        response = self._api_request(endpoint=endpoint, mode='DELETE')
+        return response
