30b842a009e3d20e9c320aa521f0b1c23afa0257
iamkubi <iamkubi@gmail.com>
2021-12-12 21:05:05 -0800

    Add backups APIs. Organize tests into folders.

--- pydactyl/api/client/servers/backups.py
+++ pydactyl/api/client/servers/backups.py
@@ -4,7 +4,7 @@ from pydactyl.api import base
 class Backups(base.PterodactylAPI):
     """Pterodactyl Client Server Backups API."""
 
-    def list_backups(self, server_id):
+    def list_backups(self, server_id: str):
         """List files belonging to specified server.
 
         Optionally specify a directory and only return results in the
@@ -17,18 +17,46 @@ class Backups(base.PterodactylAPI):
         response = self._api_request(endpoint=endpoint)
         return response
 
-    def create_backup(self):
-        """TODO"""
-        pass
+    def create_backup(self, server_id: str):
+        """Create a new backup of the specified server.
 
-    def get_backup_detail(self, backup_id):
-        """TODO"""
-        pass
+        Args:
+            server_id(str): Server identifier (abbreviated UUID)
+        """
+        endpoint = 'client/servers/{}/backups'.format(server_id)
+        response = self._api_request(endpoint=endpoint, mode='POST')
+        return response
 
-    def get_backup_download(self, backup_id):
-        """TODO"""
-        pass
+    def get_backup_detail(self, server_id: str, backup_id: str):
+        """Retrieves information about the specified backup.
 
-    def delete_backup(self, backup_id):
-        """TODO"""
-        pass
+        Args:
+            server_id(str): Server identifier (abbreviated UUID)
+            backup_id(str): Backup identifier (long UUID)
+        """
+        endpoint = 'client/servers/{}/backups/{}'.format(server_id, backup_id)
+        response = self._api_request(endpoint=endpoint)
+        return response
+
+    def get_backup_download(self, server_id: str, backup_id: str):
+        """Generates a download link for the specified backup.
+
+        Args:
+            server_id(str): Server identifier (abbreviated UUID)
+            backup_id(str): Backup identifier (long UUID)
+        """
+        endpoint = 'client/servers/{}/backups/{}/download'.format(server_id,
+                                                                  backup_id)
+        response = self._api_request(endpoint=endpoint)
+        return response
+
+    def delete_backup(self, server_id: str, backup_id: str):
+        """Deletes the specified backup.
+
+        Args:
+            server_id(str): Server identifier (abbreviated UUID)
+            backup_id(str): Backup identifier (long UUID)
+        """
+        endpoint = 'client/servers/{}/backups/{}'.format(server_id, backup_id)
+        response = self._api_request(endpoint=endpoint, mode='DELETE')
+        return response
---
+++ tests/client/backups_test.py
@@ -0,0 +1,58 @@
+import unittest
+from unittest import mock
+
+from pydactyl import PterodactylClient
+
+
+class BackupsTests(unittest.TestCase):
+
+    def setUp(self):
+        self.api = PterodactylClient(url='dummy', api_key='dummy')
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_list_backups(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/fds173/files/list',
+        }
+        self.api.client.servers.backups.list_backups('fds173')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_create_backup(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/abc123/backups',
+            'mode': 'POST',
+        }
+        self.api.client.servers.backups.create_backup('abc123')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_get_backup_detail(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/abc123/backups/longid1111',
+        }
+        self.api.client.servers.backups.get_backup_detail('abc123',
+                                                          'longid1111')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_get_backup_download(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/abc123/backups/longid1111/download',
+        }
+        self.api.client.servers.backups.get_backup_download('abc123',
+                                                            'longid1111')
+        mock_api.assert_called_with(**expected)
+
+    @mock.patch('pydactyl.api.base.PterodactylAPI._api_request')
+    def test_delete_backup(self, mock_api):
+        expected = {
+            'endpoint': 'client/servers/abc123/backups/longid1111',
+            'mode': 'DELETE',
+        }
+        self.api.client.servers.backups.delete_backup('abc123', 'longid1111')
+        mock_api.assert_called_with(**expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
