17f3d19e2a4cf660e3f2fb6910da6bb40fe1c52b
Dimoka113 <133571582+Dimoka113@users.noreply.github.com>
2025-08-16 03:11:52 +0400

    Improvement of rename_server
    Added the ability to specify a description

--- pydactyl/api/client/servers/settings.py
+++ pydactyl/api/client/servers/settings.py
@@ -4,14 +4,23 @@ from pydactyl.api import base
 class Settings(base.PterodactylAPI):
     """Pterodactyl Client Server Settings API."""
 
-    def rename_server(self, server_id: str, name: str):
+    def rename_server(self, server_id: str, name: str, description: str | None = None):
         """Renames the server.
 
         Args:
             server_id(str): Server identifier (abbreviated UUID)
             name(str): New name for the server
+            description(str|None) New description for the server (if description None, not change)
         """
-        data = {'name': name}
+
+        if name == "":
+            raise Exception('name can not been "empty"')
+
+        if description is None:
+            data = {'name': name}
+        else:
+            data = {'name': name, 'description': description}
+
         endpoint = 'client/servers/{}/settings/rename'.format(server_id)
         response = self._api_request(endpoint=endpoint, mode='POST', data=data)
         return response
