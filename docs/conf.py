6e547babf82bbc64943132fa0fd73297a846d5b2
kubi <iamkubi@gmail.com>
2025-12-15 22:59:56 -0800

    Add RTD theme back
    Something broke the rendering on RTD even though it works locally.

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
